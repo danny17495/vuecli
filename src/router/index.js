5734f1f3751958465c4daaccffd037eee99887eb
danny17495 <danny17495@gmail.com>
2020-07-03 10:17:57 +0800

    router 新增分頁

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router';
 //官方元件
 
 import Home from '@/components/HelloWorld'
+import Page from '@/components/pages/page'
+
 //自訂分頁元件
 
 Vue.use(VueRouter);
@@ -13,6 +15,11 @@ export default new VueRouter({
             name: '首頁', // 元件呈現的名稱
             path: '/index', //對應虛擬路徑
             component: Home, //對應元件
+        },
+        {
+            name: '分頁', // 元件呈現的名稱
+            path: '/page', //對應虛擬路徑
+            component: Page, //對應元件
         }
     ]
 })
